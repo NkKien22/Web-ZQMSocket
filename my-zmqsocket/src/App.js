83445778f70083cc08c3954af44c3835cc9b4ac3
Khắc Kiên <93610568+NkKien22@users.noreply.github.com>
2022-11-12 11:47:18 +0700

    update

--- my-zmqsocket/src/App.js
+++ my-zmqsocket/src/App.js
@@ -1,23 +1,18 @@
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { Header } from './components/header';
-import { ProductDetail } from './pages/productDetail';
-import { Banner } from './components/banner';
-import { ProductList } from './components/productList';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState } from 'react';
-import jwt_decode from 'jwt-decode';
-import Cookies from 'js-cookie';
-import { TOKEN_KEY } from './utils/common';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ProductDetail } from "./pages/productDetail";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Home } from "./components/home";
+import { Header } from "./components/header";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+import { TOKEN_KEY } from "./utils/common";
+import { Cart } from "./pages/cart";
 
 function App() {
   const [loginInfo, setLoginInfo] = useState();
   const [isLogined, setIsLogined] = useState();
-  const [dataSearch, setDataSearch] = useState(null);
 
   useEffect(() => {
     const tokenLogin = Cookies.get(TOKEN_KEY);
@@ -30,16 +25,19 @@ function App() {
     }
   }, []);
   return (
-    <div className="App">
-      <Header loginInfo={loginInfo} isLogined={isLogined} setDataSearch={setDataSearch} />
-      <Banner />
-      <ProductList dataSearch={dataSearch} />
-      <BrowserRouter>
+    <BrowserRouter>
+      <div>
+        <Header loginInfo={loginInfo} isLogined={isLogined} />
         <Routes>
-          <Route path="/product/:1" element={<ProductDetail />}/>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/product/:variantID"
+            element={<ProductDetail userId={loginInfo?.id} />}
+          />
+          <Route path="/cart" element={<Cart userId={loginInfo?.id} />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
