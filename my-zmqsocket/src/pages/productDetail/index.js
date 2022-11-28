b3cc40c1529b52e86f2afcc2342101e04dd905dd
Khắc Kiên <93610568+NkKien22@users.noreply.github.com>
2022-11-28 09:50:28 +0700

    update code cart

--- my-zmqsocket/src/pages/productDetail/index.js
+++ my-zmqsocket/src/pages/productDetail/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import { Button, notification } from "antd";
+import { Button, notification, Select } from "antd";
 import ReactImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import { formatPrice, URL_API } from "../../utils/common";
@@ -12,6 +12,12 @@ export const ProductDetail = (props) => {
   const { userId, countProduct, setCountProduct } = props;
   let { productID } = useParams();
   const [data, setData] = useState();
+  const [colorOption, setColorOption] = useState([]);
+  const [colorSelected, setColorSelected] = useState();
+  const [capaCitySelected, setCapaCitySelected] = useState();
+  const [capaCityOption, setCapaCityOption] = useState([]);
+  const [optionAddToCart, setOptionAddToCart] = useState([]);
+
   const [images, setImages] = useState([
     {
       original:
@@ -38,6 +44,22 @@ export const ProductDetail = (props) => {
       .get(`${URL_API}/Product/get-product-detail-by-id?id=${productID}`)
       .then((res) => {
         setData(res.data.item);
+        const colors = res.data.item.options.filter(
+          (x) => x.optionNameID === 1
+        );
+        setColorOption(colors);
+        setColorSelected(colors.map((x) => ({
+          value: x.optionValueID,
+          label: x.optionValue,
+        }))[0]);
+        const capaCities = res.data.item.options.filter(
+          (x) => x.optionNameID === 2
+        );
+        setCapaCityOption(capaCities);
+        setCapaCitySelected(capaCities.map((x) => ({
+          value: x.optionValueID,
+          label: x.optionValue,
+        }))[0]);
       });
   };
 
@@ -45,29 +67,58 @@ export const ProductDetail = (props) => {
     const payload = {
       variantId: productID,
       userId: userId,
+      options: optionAddToCart.map((x) => ({
+        optionNameID: x.optionNameID,
+        optionValueID: x.optionValueID
+      }))
     };
-    axios.post(`${URL_API}/CartItem/cart-item`, payload).then((res) => {
-      if (res) {
-        setCountProduct(countProduct + 1);
-        notification.success({
-          message: "Them vào giỏ hàng thành công",
-        });
-      }
-    }).then(() => {
-      window.location.reload();
-    });
+    if (!userId) {
+      notification.warning({
+        message: "Bạn phải đăng nhập để thêm sản phẩm vào giỏ hàng",
+      });
+      return;
+    }
+    axios
+      .post(`${URL_API}/CartItem/cart-item`, payload)
+      .then((res) => {
+        if (res) {
+          setCountProduct(countProduct + 1);
+          notification.success({
+            message: "Them vào giỏ hàng thành công",
+          });
+        }
+      })
+      .then(() => {
+        window.location.reload();
+      });
   };
+
+  useEffect(() => {
+    if (colorSelected && capaCitySelected) {
+      const data = [];
+      data.push(colorOption.find((x) => x.optionValueID === colorSelected.value));
+      data.push(capaCityOption.find((x) => x.optionValueID === capaCitySelected.value));
+      setOptionAddToCart(data);
+    }
+  }, [colorSelected, capaCitySelected]);
 
   useEffect(() => {
     getProduct();
   }, []);
 
   useEffect(() => {
-    console.log(countProduct);
     if (isNumber(countProduct)) {
       setCountProduct(localStorage.setItem("COUNT_PRODUCT", countProduct));
     }
   }, [countProduct]);
+
+  const handleChange = (value) => {
+    setColorSelected(colorOption.find((x) => x.value === value));
+  };
+
+  const handleChangeCapacity = (value) => {
+    setCapaCitySelected(capaCityOption.find((x) => x.value === value));
+  };
 
   return (
     <div className="container mt-5">
@@ -85,14 +136,35 @@ export const ProductDetail = (props) => {
             </h4>
           </div>
           <div className="ms-5">
-            {data?.options?.map((x) => (
-              <div>
-                {x.optionName}: {x.optionValue}
-              </div>
-            ))}
+            Màu:{" "}
+            <Select
+              style={{
+                width: 120,
+              }}
+              value={colorSelected}
+              onChange={handleChange}
+              options={colorOption.map((x) => ({
+                value: x.optionValueID,
+                label: x.optionValue,
+              }))}
+            />
           </div>
           <div className="ms-5 mt-2">
-            <Button size="large" type="primary" onClick={addToCart}>
+            RAM:{" "}
+            <Select
+              style={{
+                width: 120,
+              }}
+              value={capaCitySelected}
+              onChange={handleChangeCapacity}
+              options={capaCityOption.map((x) => ({
+                value: x.optionValueID,
+                label: x.optionValue,
+              }))}
+            />
+          </div>
+          <div className="ms-5 mt-2">
+            <Button size="large" type="primary" onClick={addToCart} disabled={!data?.isProductVariantEnabled}>
               Thêm vào giỏ hàng
             </Button>
           </div>
